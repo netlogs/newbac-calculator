bb87da3461ed9b7a65967935dcf5814d04a906e8
netlogs <2406292852@qq.com>
2024-11-18 14:25:41 +0800

    add typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
